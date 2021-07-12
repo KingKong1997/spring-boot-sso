6687f51b5aab1315af9dda9c093f3d10b93bd485
yzd <yzd@ktc.com>
2021-07-12 17:00:31 +0800

    test

--- sso-server/src/main/java/com/example/sso/server/web/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.sso.server.web.service;
-
-import com.example.sso.server.web.param.LoginParam;
-
-public interface UserService {
-    LoginParam checkoutUser(String name, String password);
-}
